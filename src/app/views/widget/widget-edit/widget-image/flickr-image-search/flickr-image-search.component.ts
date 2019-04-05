3b350f34aad4cf07590767be1b18dbb1a1ffab48
sunyang <sun.yang@husky.neu.edu>
2019-04-05 02:07:39 -0700

    fix flicker route

--- src/app/views/widget/widget-edit/widget-image/flickr-image-search/flickr-image-search.component.ts
+++ src/app/views/widget/widget-edit/widget-image/flickr-image-search/flickr-image-search.component.ts
@@ -64,7 +64,7 @@ export class FlickrImageSearchComponent implements OnInit {
         (data: any) => {
           const result = data;
           if (result) {
-            this.router.navigate(['/#/website/' + this.wid + '/page/' + this.pid + '/widget/' + this.widget._id] );
+            this.router.navigate(['/website/', this.wid, 'page', this.pid, 'widget', this.widget._id] );
           } else {
             this.error = 'failed!';
           }
